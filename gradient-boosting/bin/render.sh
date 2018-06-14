346b2ea79c74602a5a2d05f9f946943045885c65
parrt <parrt@cs.usfca.edu>
2018-06-14 14:12:28 -0700

    tweak

--- gradient-boosting/bin/render.sh
+++ gradient-boosting/bin/render.sh
@@ -8,6 +8,7 @@ do
 	if test $I/css/article.css -nt $O/L2-loss.html || \
            test $I/L2-loss.xml -nt $O/L2-loss.html || \
            test $I/L1-loss.xml -nt $O/L1-loss.html || \
+           test $I/faq.xml -nt $O/faq.html || \
            test $I/descent.xml -nt $O/descent.html
 	then
 		java -jar /Users/parrt/github/bookish/target/bookish-1.0-SNAPSHOT.jar -target html -o /tmp/gradient-boosting $I/article.xml
