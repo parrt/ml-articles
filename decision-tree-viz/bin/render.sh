d3e7834dc7f0907f74e36c5b75d9a188efc14b7b
parrt <parrt@cs.usfca.edu>
2018-09-04 15:05:32 -0700

    initial add

---
+++ decision-tree-viz/bin/render.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+I="/Users/parrt/github/ml-articles/decision-tree-viz/"
+O="/tmp/decision-tree-viz"
+
+while true
+do
+	if test $I/css/article.css -nt $O/L2-loss.html || \
+           test $I/index.xml -nt $O/index.html
+	then
+		java -jar /Users/parrt/github/bookish/target/bookish-1.0-SNAPSHOT.jar -target html -o $O $I/article.xml
+	fi
+	sleep .2s
+done
