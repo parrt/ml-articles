70880d8f673603ec5d159318639c1d80b96742c8
parrt <parrt@cs.usfca.edu>
2020-09-26 14:39:15 -0700

    start of new article

---
+++ tensor-sensor/bin/render.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+I="/Users/parrt/github/ml-articles/tensor-sensor/"
+O="/tmp/tensor-sensor"
+
+while true
+do
+	if test $I/css/article.xml -nt $O/index.html || \
+	   test $I/css/article.css -nt $O/index.html || \
+           test $I/index.xml -nt $O/index.html
+	then
+		java -jar /Users/parrt/github/bookish/target/bookish-1.0-SNAPSHOT.jar -target html -o $O $I/article.xml
+	fi
+	sleep .2s
+done
