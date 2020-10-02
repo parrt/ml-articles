7b62d29d0364a138398cbe6d69b537d1496b0d47
parrt <parrt@cs.usfca.edu>
2020-10-02 15:30:44 -0700

    working on tensor article

--- tensor-sensor/bin/watch.sh
+++ tensor-sensor/bin/watch.sh
@@ -1,1 +1,1 @@
-fswatch -o ~/github/ml-articles/tensor-sensor/index.html | xargs -n1 -I {} osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
+fswatch -o /tmp/tensor-sensor/index.html | xargs -n1 -I {} osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
